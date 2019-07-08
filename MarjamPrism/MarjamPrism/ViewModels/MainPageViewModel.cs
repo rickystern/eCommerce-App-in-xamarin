194b080aa507c2ab9a64014b978de7e28b0c27a7
Shakeane Hinds <SHinds@realdecoy.com>
2019-07-08 17:22:20 -0500

    Added search functionality and updated the look of the voice and cart pages

--- MarjamPrism/MarjamPrism/ViewModels/MainPageViewModel.cs
+++ MarjamPrism/MarjamPrism/ViewModels/MainPageViewModel.cs
@@ -19,6 +19,13 @@ namespace MarjamPrism.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private string _searchString;
+        public string SearchString
+
+        {
+            get { return _searchString; }
+            set { SetProperty(ref _searchString, value); }
+        }
         private PermissionStatus status;
         private Product _selectedProduct;
         public Product SelectedProduct
@@ -44,14 +51,42 @@ namespace MarjamPrism.ViewModels
 
         public DelegateCommand ItemSelectedCommand { get; }
 
+        public DelegateCommand SearchCommand { get; }
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Home";
             ItemSelectedCommand = new DelegateCommand(async () => await ProductSelectedHandler());
+            SearchString = "";
+            SearchCommand = new DelegateCommand(async () => await SearchHandlerAsync());
 
         }
 
+        private async Task SearchHandlerAsync()
+        {
+            var resp = await new HttpClient().GetStringAsync("https://api.bestbuy.com/v1/products((search="+SearchString+ "))?apiKey=wgd9fp6cujtdn27wm9k8rtdg&sort=regularPrice.dsc&show=image,name,regularPrice,sku,shortDescription,inStoreAvailability,manufacturer&pageSize=30&format=json");
+
+            var bestBuyResult = JsonConvert.DeserializeObject<BestBuyResult>(resp);
+
+
+            Products = new ObservableCollection<Product>(bestBuyResult.Products);
+            foreach (var item in Products)
+            {
+                item.Name = item.Name.Substring(0, item.Name.Length/2) + "...";
+                item.RegularPrice = "$" + item.RegularPrice;
+                if (item.InStoreAvailability == "true")
+                {
+                    item.InStoreAvailability = "In Stock";
+                }
+
+                else
+                {
+                    item.InStoreAvailability = "Out of Stock";
+                }
+
+            }
+        }
 
         private async Task ProductSelectedHandler()
         {
@@ -64,13 +99,16 @@ namespace MarjamPrism.ViewModels
         {
 
             var resp = await new HttpClient().GetStringAsync("https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=wgd9fp6cujtdn27wm9k8rtdg&sort=regularPrice.dsc&show=image,inStoreAvailability,manufacturer,regularPrice,shortDescription,name,sku&pageSize=30&format=json");
+            //var resp = await new HttpClient().GetStringAsync("https://api.bestbuy.com/v1/products((categoryPath.id=abcat0204000))?apiKey=wgd9fp6cujtdn27wm9k8rtdg&sort=regularPrice.dsc&show=image,name,regularPrice,sku,shortDescription,inStoreAvailability,manufacturer&pageSize=30&format=json");
+            //var resp = await new HttpClient().GetStringAsync("https://api.bestbuy.com/v1/products((search=laptop))?apiKey=wgd9fp6cujtdn27wm9k8rtdg&sort=regularPrice.dsc&show=image,name,regularPrice,sku,shortDescription,inStoreAvailability,manufacturer&format=json");
+
             var bestBuyResult = JsonConvert.DeserializeObject<BestBuyResult>(resp);
 
             
             Products = new ObservableCollection<Product>(bestBuyResult.Products);
             foreach (var item in Products)
             {
-                item.Name = item.Name.Substring(0, 60) + "...";
+                item.Name = item.Name.Substring(0, item.Name.Length / 2) + "...";
                 item.RegularPrice = "$" + item.RegularPrice;
                 if (item.InStoreAvailability == "true")
                 {
